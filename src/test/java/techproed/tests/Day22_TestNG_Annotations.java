dd7f49b4897ae0625f82e7c168f3a145da37d423
saidErsoz <m.said.ersoz3443@gmail.com>
2023-04-25 13:10:38 +0300

    20/04/2023

---
+++ src/test/java/techproed/tests/Day22_TestNG_Annotations.java
@@ -0,0 +1,41 @@
+package techproed.tests;
+
+import org.testng.annotations.Test;
+
+public class Day22_TestNG_Annotations {
+
+    @Test
+    public void test01() {
+        System.out.println("Test 1");
+    }
+
+    @Test
+    public void test02() {
+        System.out.println("Test 2");
+    }
+
+    @Test
+    public void test03() {
+        System.out.println("Test 3");
+    }
+
+    @Test
+    public void test04() {
+        System.out.println("Test 4");
+    }
+
+    @Test
+    public void test05() {
+        System.out.println("Test 5");
+    }
+
+    @Test
+    public void test06() {
+        System.out.println("Test 6");
+    }
+
+    @Test
+    public void test07() {
+        System.out.println("Test 7");
+    }
+}
