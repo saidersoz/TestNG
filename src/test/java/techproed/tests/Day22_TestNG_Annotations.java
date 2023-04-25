9e81604f16e878b834ebee0b5ad9964d585816aa
saidErsoz <m.said.ersoz3443@gmail.com>
2023-04-25 13:47:44 +0300

    20/04/2023

--- src/test/java/techproed/tests/Day22_TestNG_Annotations.java
+++ src/test/java/techproed/tests/Day22_TestNG_Annotations.java
@@ -8,10 +8,20 @@ public class Day22_TestNG_Annotations {
     @Test : Test case oluşturmak için kullanılır.
         @Before ve @After : 5 Before ve 5 After anotation bulunur.
         --> Suite > Tests > Groups > Class > Method
-            @BeforeSuite : Herbir test suite'inden önce 1 kez çalışır.
-            @AfterSuite : Herbir test suite'inden sonra 1 kez çalışır.
-            @BeforeTest : Herbir test öncesi 1 kez çalışır. (Herbir test case'i öncesi değil!!!)
+                  @BeforeSuite : Herbir test suite'inden önce 1 kez çalışır.
+                  @AfterSuite : Herbir test suite'inden sonra 1 kez çalışır.
 
+                  @BeforeTest : Test öncesi 1 kez çalışır. (Herbir test case'i öncesi değil!!!)
+                  @BeforeTest : Test sonrası 1 kez çalışır. (Herbir test case'i öncesi değil!!!)
+
+                  @BeforeGroups : Özel belirtilmiş herhangi bir grup öncesi çalışır.
+                  @AfterGroups : Özel belirtilmiş herhangi bir grup sonrası çalışır.
+
+                  @BeforeClass : Her bir class'dan önce bir kez çalışır
+                  @AfterClass : Her bir class'dan sonra bir kez çalışır
+
+                  @Before : Herbir test methodu öncesinde 1 kere çalışır
+                  @After : Herbir test methodu sonrasında 1 kere çalışır
      */
 
     @BeforeSuite
@@ -34,6 +44,35 @@ public class Day22_TestNG_Annotations {
         System.out.println("After Test");
     }
 
+    @BeforeGroups
+    public void beforeGroups(){
+        System.out.println("Before Groups");
+    }
+
+    @AfterGroups
+    public void afterGroups(){
+        System.out.println("After Groups");
+    }
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("Before Class");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("After Class");
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        System.out.println("Before");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        System.out.println("After");
+    }
 
     @Test
     public void test01() {
@@ -45,7 +84,7 @@ public class Day22_TestNG_Annotations {
         System.out.println("Test 2");
     }
 
-    @Test
+    @Test(groups = "regression-test")
     public void test03() {
         System.out.println("Test 3");
     }
@@ -55,7 +94,7 @@ public class Day22_TestNG_Annotations {
         System.out.println("Test 4");
     }
 
-    @Test
+    @Test(groups = "regression-test")
     public void test05() {
         System.out.println("Test 5");
     }
