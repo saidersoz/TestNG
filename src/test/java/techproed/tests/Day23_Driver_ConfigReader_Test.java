9a1ee519e16ddb3abf386df7b91b3de5fd07b738
saidErsoz <m.said.ersoz3443@gmail.com>
2023-04-27 13:15:13 +0300

    27/04/2023

--- src/test/java/techproed/tests/Day23_Driver_ConfigReader_Test.java
+++ src/test/java/techproed/tests/Day23_Driver_ConfigReader_Test.java
@@ -1,7 +1,21 @@
 package techproed.tests;
 
-public class Day23_Driver_ConfigReader_Test {
+import org.testng.annotations.Test;
+import techproed.utilities.Driver;
 
+import static org.testng.Assert.assertTrue;
 
+public class Day23_Driver_ConfigReader_Test{
 
+    @Test
+    public void testDriver() {
+        //"https://techproeducation.com/" sayfasına gidiniz.
+        Driver.getDriver().get("https://techproeducation.com/");
+
+        //Title testi yapınız
+        assertTrue(Driver.getDriver().getTitle().contains("Techpro"));
+
+        //Sayfayı kapatınız
+        Driver.closeDriver();
+    }
 }
