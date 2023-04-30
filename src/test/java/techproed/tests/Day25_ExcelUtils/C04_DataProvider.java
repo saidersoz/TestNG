e1e619c30593247babfdce823dae4865eb7cae51
saidErsoz <m.said.ersoz3443@gmail.com>
2023-04-30 20:20:09 +0300

    30/04/2023

---
+++ src/test/java/techproed/tests/Day25_ExcelUtils/C04_DataProvider.java
@@ -0,0 +1,45 @@
+package techproed.tests.Day25_ExcelUtils;
+
+import org.openqa.selenium.devtools.v85.network.model.DataReceived;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import techproed.utilities.ConfigReader;
+import techproed.utilities.Driver;
+
+public class C04_DataProvider {
+
+    /*
+        -Data Provider bir çok veriyi test case'lere loop kullanmadan aktarmak için kullanılır.
+        TestNG den gelen bir özelliktir. 2 Boyutlu bir Object Array return eder.
+         DDT(Data Driven Testing) için kullanılır. Yani 1 den fazla veriyi test case'lerde aynı
+        anda kullanmak için kullanılır.
+        Kullanımı için @Test notasyonundan sonra parametre olarak dataprovider yazılır ve
+        String bir isim belirtilir. Bu belirttiğimiz isimle @DataProvider notasyonu ile method oluşturulur.
+         */
+
+
+    @Test(dataProvider = "urunler")
+    public void testDataProvider(String data) { //DataP. 'daki verileri alabilmek için
+                                                // Test methodumuza String bir parametre ataması yaparız
+        System.out.println(data);
+    }
+
+    @DataProvider(name = "googleTest")
+    public static Object[][] urunler() {
+        return new Object[][]{{"Volvo"},{"Mercedes"},{"Audi"},{"Honda"},{"Toyota"},{"Opel"},{"BMW"}};
+    }
+
+    /*
+    Eğer farklı bir test method'u için aynı dataProvider method'u kullanılacaksa
+    @DataProvider(name = "googleTest") şeklinde dataprovider notasyonundan sonra name parametresie oluşturduğumuz
+    method'un adını yazınız
+     */
+
+    @Test
+    public void googleTest(String araclar) {
+        //Google sayfasına gidiniz
+        Driver.getDriver().get(ConfigReader.getProperty("google_url"));
+        //{"Volvo"},{"Mercedes"},{"Audi"},{"Honda"},{"Toyota"},{"Opel"},{"BMW"} araçlarını aratınız
+        //Her aratmmadan sonra sayfa resmi alınız.
+    }
+}
